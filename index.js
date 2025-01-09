e1f82723167aef5a0718aa7480576c8b0339a4cb
Ethan Xu <ethanxu9002@gmail.com>
2025-01-09 15:06:26 -0800

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+let displayList = []
+let width = 20
+let height = 10
+
+/*█
+document.addEventListener("keypress", function(event) {
+  if (event.keyCode == 13) {
+    alert('hi.');
+  }
+});
+*/
+
+let display = document.getElementById("display-El")
+
+
+function setup() {
+    for (let step = 0; step < width*height; step++) {
+        displayList.push(".")
+    }
+}
+
+setup()
+
+display.style.width=width+"ch"
+display.innerText = displayList.join("")
+console.log(screen)
+
