37685976a05ad24e976abc0f94ffbd1ca685c32c
Ethan X <ethanxu9002@gmail.com>
2025-01-14 15:34:08 -0800

    redo from the ground up to allow for meta data assigned to each tile

--- index.js
+++ index.js
@@ -1,65 +1,18 @@
-let displayList = []
-let width = 20
-let height = 10
 
-/*█
-document.addEventListener("keypress", function(event) {
-  if (event.keyCode == 13) {
-    alert('hi.');
-  }
-});
-*/
+let metaList = []
+let displayList = []
+
+let width = 5
+let height = 3
 
 let display = document.getElementById("display-El")
 
-let atPos = 0
+
 
 function setup() {
     for (let step = 0; step < width*height; step++) {
-        displayList.push(".")
+      metaList.push("empty")  
     }
-    displayList.splice(atPos, 1, "@")
-}
-function update() {
-  display.innerText = displayList.join("")
+    //displayList.splice(atPos, 1, "@")
 }
 
-setup()
-
-//Movement
-document.addEventListener("keypress", function(press6) {
-  if (press6.key == "6"){
-    displayList.splice(atPos, 1, ".")
-    displayList.splice(atPos + 1, 1, "@")
-    atPos += 1
-  }
-  update()
-})
-document.addEventListener("keypress", function(press4) {
-  if (press4.key == "4"){
-    displayList.splice(atPos, 1, ".")
-    displayList.splice(atPos - 1, 1, "@")
-    atPos -= 1
-  }
-  update()
-})
-document.addEventListener("keypress", function(press2) {
-  if (press2.key == "2"){
-    displayList.splice(atPos, 1, ".")
-    displayList.splice(atPos + width, 1, "@")
-    atPos += width
-  }
-  update()
-})
-document.addEventListener("keypress", function(press8) {
-  if (press8.key == "8"){
-    displayList.splice(atPos, 1, ".")
-    displayList.splice(atPos - width, 1, "@")
-    atPos -= width
-  }
-  update()
-})
-
-display.style.width=width+"ch"
-display.innerText = displayList.join("")
-console.log(screen)
---
+++ test.js
@@ -0,0 +1,72 @@
+let displayList = []
+let metaList = []
+
+let width = 5
+let height = 3
+
+/*█
+document.addEventListener("keypress", function(event) {
+  if (event.keyCode == 13) {
+    alert('hi.');
+  }
+});
+*/
+
+let display = document.getElementById("display-El")
+
+
+let atPos = 0
+
+function setup() {
+    for (let step = 0; step < width*height; step++) {
+      metaList.push(".")  
+      displayList.push(".")
+    }
+    //displayList.splice(atPos, 1, "@")
+    displayList.splice(7, 1, "@")
+}
+function update() {
+  display.innerText = displayList.join("")
+}
+
+setup()
+
+//Movement
+/*
+document.addEventListener("keypress", function(press6) {
+  if (press6.key == "6"){
+    displayList.splice(atPos, 1, ".")
+    displayList.splice(atPos + 1, 1, "@")
+    atPos += 1
+  }
+  update()
+})
+document.addEventListener("keypress", function(press4) {
+  if (press4.key == "4"){
+    displayList.splice(atPos, 1, ".")
+    displayList.splice(atPos - 1, 1, "@")
+    atPos -= 1
+  }
+  update()
+})
+document.addEventListener("keypress", function(press2) {
+  if (press2.key == "2"){
+    displayList.splice(atPos, 1, ".")
+    displayList.splice(atPos + width, 1, "@")
+    atPos += width
+  }
+  update()
+})
+document.addEventListener("keypress", function(press8) {
+  if (press8.key == "8"){
+    displayList.splice(atPos, 1, ".")
+    displayList.splice(atPos - width, 1, "@")
+    atPos -= width
+  }
+  update()
+})
+*/
+
+display.style.width=width+"ch"
+display.innerText = displayList.join("")
+console.log(screen)
